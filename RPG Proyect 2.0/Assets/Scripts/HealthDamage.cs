17fe869636765c8e227b2ad2905fe0d277d419f2
aaron-3d <aarongh.garoe@gmail.com>
2022-02-10 09:39:21 +0000

    Sistema de vida añadido.
    Ahora puedes recibir daño. Tras recibir daño, recibes un segundo de invulnerabilidad y tu velocidad se ve reducida durante 0.2 segundos.

---
+++ RPG Proyect 2.0/Assets/Scripts/HealthDamage.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthDamage : MonoBehaviour
+{
+    public int vidaP = 100;
+    public bool invencible = false;
+
+    public float tiempoInvencible = 1;
+    public float tiempoFrenado = 0.2f;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void QuitarVida(int cantidad)
+    {
+        if(!invencible && vidaP > 0)
+        {
+            vidaP -= cantidad;
+            anim.Play("TakeDamage1");
+            StartCoroutine(Invulnerabilidad());
+            StartCoroutine(FrenarVelocidad());
+
+            if(vidaP == 0)
+            {
+                GameOver();
+            }
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Has muerto.");
+    }
+
+    IEnumerator Invulnerabilidad()
+    {
+        invencible = true;
+        yield return new WaitForSeconds(tiempoInvencible);
+        invencible = false;
+    }
+
+    IEnumerator FrenarVelocidad()
+    {
+        var velocidadActual = GetComponent<ThirdPersonMovement>().speed;
+        GetComponent<ThirdPersonMovement>().speed = 0;
+        yield return new WaitForSeconds(tiempoFrenado);
+        GetComponent<ThirdPersonMovement>().speed = velocidadActual;
+    }
+
+}
