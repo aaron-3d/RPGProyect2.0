816d81860f33d633529f14ea95e3d454f5cdab30
Armiboli99 <83208829+Armiboli99@users.noreply.github.com>
2022-02-18 00:17:51 +0000

    Cambios en el inventario v2

--- RPG Proyect 2.0/Assets/Scripts/Loot/Inventario.cs
+++ RPG Proyect 2.0/Assets/Scripts/Loot/Inventario.cs
@@ -5,16 +5,49 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 public class Inventario : MonoBehaviour
 {
+    [System.Serializable]
+    public struct ObjetoInvId
+    {
+        public int id;
+        public int cantidad;
+
+        public ObjetoInvId(int id, int cantidad)
+        {
+            this.id = id;
+            this.cantidad = cantidad;
+        }
+    }
+
+    [SerializeField]
+    DataBase data;
+    [Header("Variables del Drag and drop")]
     public GraphicRaycaster graphRay;
     private PointerEventData pointerData;
     private List<RaycastResult> raycastResults; //Array para almacenar el item que coges con el raycaster
-    public Transform canvas;
+    public static Transform canvas; //se vuelve static para utilizarlo en item
     public GameObject objetoSeleccionado;
     public Transform exParent;
+    [Header("Prefs e items")]
+    public static GameObject Descripcion;
+    //public CartelEliminacion CE;
+    public int OSC;
+    public int OSID;
+
+    public Transform contenido;
+    public Item item;
+    public List<ObjetoInvId> inventarioo = new List<ObjetoInvId>();
     void Start()
     {
+        InventoryUpdate();
+
         pointerData = new PointerEventData(null);
         raycastResults = new List<RaycastResult>();
+
+        Descripcion = GameObject.Find("Descripcion");
+
+        //CE.gameObject.SetActive(false);
+
+        canvas = transform.parent.transform;
     }
 
     // Update is called once per frame
@@ -24,65 +57,73 @@ public class Inventario : MonoBehaviour
     }
     void Arrastrar()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
             pointerData.position = Input.mousePosition;
             graphRay.Raycast(pointerData, raycastResults);
-            if(raycastResults.Count > 0)
+            if (raycastResults.Count > 0)
             {
                 if (raycastResults[0].gameObject.GetComponent<Item>()) //si el objeto que pulsamos tiene el componente Item
                 {
                     objetoSeleccionado = raycastResults[0].gameObject;
+
+                    OSC = objetoSeleccionado.GetComponent<Item>().cantidad;
+                    OSID = objetoSeleccionado.GetComponent<Item>().ID;
+
                     exParent = objetoSeleccionado.transform.parent.transform; //para guardar la posicion principal del item al pulsarlo
+                    exParent.GetComponent<Image>().fillCenter = false; //borrar el fillCenter de las casillas donde no haya nada
                     objetoSeleccionado.transform.SetParent(canvas); //desenparentar el item del slit
                 }
             }
         }
 
-        if(objetoSeleccionado != null)
+        if (objetoSeleccionado != null)
         {
             objetoSeleccionado.GetComponent<RectTransform>().localPosition = CanvasScreen(Input.mousePosition); //Que se vea el item moviendose junto al raton
         }
-        if (Input.GetMouseButtonUp(0))
+        if (objetoSeleccionado != null)
         {
-            pointerData.position = Input.mousePosition;
-            raycastResults.Clear(); //eliminar los resultados del RayCast
-            graphRay.Raycast(pointerData, raycastResults); //para saber donde dejamos el item
-            if(raycastResults.Count > 0)
+            if (Input.GetMouseButtonUp(1))
             {
-                foreach(var resultado in raycastResults)
+                pointerData.position = Input.mousePosition;
+                raycastResults.Clear(); //eliminar los resultados del RayCast
+                graphRay.Raycast(pointerData, raycastResults); //para saber donde dejamos el item
+                if (raycastResults.Count > 0)
                 {
+                    foreach (var resultado in raycastResults)
+                    {
 
-                    if(resultado.gameObject.CompareTag("Slot")) //caso de slot libre
-                    {
-                        if (resultado.gameObject.GetComponentInChildren<Item>() == null)
+                        if (resultado.gameObject.CompareTag("Slot")) //caso de slot libre
                         {
-                            objetoSeleccionado.transform.SetParent(resultado.gameObject.transform);
-                            objetoSeleccionado.transform.localPosition = Vector2.zero;
-                            exParent = objetoSeleccionado.transform.parent.transform;
-                        }
-                        else //caso de slot ocupado
-                        {
-                            if(resultado.gameObject.GetComponentInChildren<Item>().ID == objetoSeleccionado.GetComponent<Item>().ID) //acceder al ID y si es igual
+                            if (resultado.gameObject.GetComponentInChildren<Item>() == null)
                             {
-                                resultado.gameObject.GetComponentInChildren<Item>().cantidad += objetoSeleccionado.GetComponent<Item>().cantidad; //suma las cantidadesd e un mismo item, (2 pociones + 2 pociones)
-                                Destroy(objetoSeleccionado.gameObject);
-                            }
-                            else //si el ID es distinto
-                            {
-                                objetoSeleccionado.transform.SetParent(exParent.transform);
+                                objetoSeleccionado.transform.SetParent(resultado.gameObject.transform);
                                 objetoSeleccionado.transform.localPosition = Vector2.zero;
+                                exParent = objetoSeleccionado.transform.parent.transform;
+                            }
+                            else //caso de slot ocupado
+                            {
+                                if (resultado.gameObject.GetComponentInChildren<Item>().ID == objetoSeleccionado.GetComponent<Item>().ID) //acceder al ID y si es igual
+                                {
+                                    resultado.gameObject.GetComponentInChildren<Item>().cantidad += objetoSeleccionado.GetComponent<Item>().cantidad; //suma las cantidadesd e un mismo item, (2 pociones + 2 pociones)
+                                    Destroy(objetoSeleccionado.gameObject);
+                                }
+                                else //si el ID es distinto
+                                {
+                                    objetoSeleccionado.transform.SetParent(exParent.transform);
+                                    objetoSeleccionado.transform.localPosition = Vector2.zero;
+                                }
                             }
                         }
-                    }
-                    else
-                    {
-                        objetoSeleccionado.transform.SetParent(exParent.transform);
-                        objetoSeleccionado.transform.localPosition = Vector2.zero;
+                        else
+                        {
+                            objetoSeleccionado.transform.SetParent(exParent.transform);
+                            objetoSeleccionado.transform.localPosition = Vector2.zero;
+                        }
                     }
                 }
+                objetoSeleccionado = null;
             }
-            objetoSeleccionado = null;
         }
         raycastResults.Clear();
     }
@@ -93,4 +134,160 @@ public class Inventario : MonoBehaviour
 
         return (new Vector2(viewportPoint.x * canvasSize.x, viewportPoint.y * canvasSize.y) - (canvasSize / 2)); //multiplica la cantidad de pixeles para dividirla y tener un punto intermedio
     }
+
+    public void AgregarItem(int id, int cantidad)
+    {
+        for (int i = 0; i < inventarioo.Count; i++)
+        {
+            if (inventarioo[i].id == id && data.baseDatos[id].acumulable) //para agregar los objetos acumulables dentro del mismo stack (Eliminar lo siguiente del && y la bool acumulable si no queremos que se stackeen)
+            {
+                inventarioo[i] = new ObjetoInvId(inventarioo[i].id, inventarioo[i].cantidad + cantidad);
+                InventoryUpdate();
+                return;
+            }
+        }
+        if (!data.baseDatos[id].acumulable)
+        {
+            inventarioo.Add(new ObjetoInvId(id, 1));
+        }
+        else
+        {
+            inventarioo.Add(new ObjetoInvId(id, cantidad));
+        }
+        InventoryUpdate();
+    }
+
+    public void EliminarItem(int id, int cantidad)
+    {
+        for (int i = 0; i < inventarioo.Count; i++)
+        {
+            if (inventarioo[i].id == id)
+            {
+                inventarioo[i] = new ObjetoInvId(inventarioo[i].id, inventarioo[i].cantidad - cantidad);
+                if (inventarioo[i].cantidad <= 0)
+                {
+                    inventarioo.Remove(inventarioo[i]);
+                    InventoryUpdate();
+                    break;
+                }
+                InventoryUpdate();
+            }
+        }
+    }
+
+    List<Item> pool = new List<Item>();
+    public void InventoryUpdate()
+    {
+        for (int i = 0; i < pool.Count; i++)                    //hasta obtener el mismo numero en i que en pool.count
+        {
+            if (i < inventarioo.Count)                         //si hay menos scripts que objetos de inventario
+            {
+                ObjetoInvId o = inventarioo[i];
+                pool[i].ID = o.id;                              //aqui le asigno la id del objeto a mi script llamada Item
+                pool[i].GetComponent<Image>().sprite = data.baseDatos[o.id].icono;
+                pool[i].GetComponent<RectTransform>().localPosition = Vector3.zero;
+                pool[i].cantidad = o.cantidad;
+                pool[i].Boton.onClick.RemoveAllListeners();
+                pool[i].Boton.onClick.AddListener(() => gameObject.SendMessage(data.baseDatos[o.id].Void, SendMessageOptions.DontRequireReceiver));
+                pool[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                pool[i].gameObject.SetActive(false);
+                pool[i]._descripcion.SetActive(false);
+                pool[i].gameObject.transform.parent.GetComponent<Image>().fillCenter = false;
+            }
+        }
+        if (inventarioo.Count > pool.Count)
+        {
+            for (int i = pool.Count; i < inventarioo.Count; i++)
+            {
+                Item it = Instantiate(item, contenido.GetChild(i)); // aqui el getchild lo utilizo para crear el item dentro del slot
+                pool.Add(it);
+
+                if (contenido.GetChild(0).childCount >= 2)
+                {
+                    for (int s = 0; s < contenido.childCount; s++)
+                    {
+                        if (contenido.GetChild(s).childCount == 0)
+                        {
+                            it.transform.SetParent(contenido.GetChild(s));
+                            break;
+                        }
+                    }
+                }
+                it.transform.position = Vector3.zero;
+                it.transform.localScale = Vector3.one;
+
+                ObjetoInvId o = inventarioo[i];
+                pool[i].ID = o.id;                                                              //Aqui le asigno la id del objeto a mi script llamado Item
+                pool[i].GetComponent<RectTransform>().localPosition = Vector3.zero;
+                pool[i].GetComponent<Image>().sprite = data.baseDatos[o.id].icono;
+                pool[i].cantidad = o.cantidad;
+                pool[i].Boton.onClick.RemoveAllListeners();
+                pool[i].Boton.onClick.AddListener(() => gameObject.SendMessage(data.baseDatos[o.id].Void, SendMessageOptions.DontRequireReceiver));
+                pool[i].gameObject.SetActive(true);
+            }
+        }
+    }
+    void PocionSalud()
+    {
+
+    }
+    void Espada()
+    {
+
+    }
+
+    void Casco()
+    {
+
+    }
+
+    void Pechera()
+    {
+
+    }
+
+    void Pantalones()
+    {
+
+    }
+
+    void Botas()
+    {
+
+    }
+
+    void Hacha()
+    {
+
+    }
+    void Daga()
+    {
+
+    }
+
+    void PUVelocidadMovimiento()
+    {
+
+    }
+
+    void PUVelocidadAtaque()
+    {
+
+    }
+    void PUSalto()
+    {
+
+    }
+    void PUVida()
+    {
+
+    }
+    void PUDaño()
+    {
+
+    }
+
 }
--- RPG Proyect 2.0/Assets/Scripts/Loot/Item.cs
+++ RPG Proyect 2.0/Assets/Scripts/Loot/Item.cs
@@ -2,20 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using TMPro;
 
-public class Item : MonoBehaviour
+public class Item : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler
 {
     public int cantidad = 1; //cantidad de cada objeto (20 pociones)
-    public Text textoCantidad;
+    public Text cantidadTexto;
     public int ID;
+    public bool acumulable;
+    public Button Boton;
+    public GameObject _descripcion;
+    public TextMeshPro Nombre_;
+    public TextMeshPro Dato_;
+    public Vector3 offset;
+    public DataBase DB;
     void Start()
     {
-        
+        acumulable = DB.baseDatos[ID].acumulable;
+        Boton = GetComponent<Button>();
+        _descripcion = Inventario.Descripcion;
+        Nombre_ = _descripcion.transform.GetChild(0).GetComponent<TextMeshPro>();
+        Dato_ = _descripcion.transform.GetChild(1).GetComponent<TextMeshPro>();
+        _descripcion.SetActive(false);
+        if (!_descripcion.GetComponent<Image>().enabled) //en el caso de que el componente image no este activado, lo voy a activar.
+        {
+            _descripcion.GetComponent<Image>().enabled = true;
+            Nombre_.enabled = true;
+            Dato_.enabled = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        textoCantidad.text = cantidad.ToString();
+        //textoCantidad.text = cantidad.ToString();
+        if(transform.parent.GetComponent<Image>() != null)
+        {
+            transform.parent.GetComponent<Image>().fillCenter = true;
+        }
+
+        cantidadTexto.text = cantidad.ToString();
+
+        if(transform.parent == Inventario.canvas)
+        {
+            _descripcion.SetActive(false);
+        }
+        
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _descripcion.SetActive(true);
+        Nombre_.text = DB.baseDatos[ID].nombre;
+        Dato_.text = DB.baseDatos[ID].descripcion;
+        _descripcion.transform.position = transform.position + offset;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _descripcion.SetActive(false);
     }
 }
