4014597cf6f8e8330a6cd99226b0d604e4432c65
Armiboli99 <83208829+Armiboli99@users.noreply.github.com>
2022-02-24 13:23:15 +0000

    Probabilidad de dropear items

--- RPG Proyect 2.0/Assets/Scripts/Loot/ItemSuelto.cs
+++ RPG Proyect 2.0/Assets/Scripts/Loot/ItemSuelto.cs
@@ -8,21 +8,37 @@ public class ItemSuelto : MonoBehaviour
     public int cantidad;
     public int ID;
     public Inventario Inv;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            ItemSuelto itSuelto = Inv.itemsSueltos.Find(x => x.ID == this.ID);
             Inv.AgregarItem(ID, cantidad);
+
+            if(itSuelto != null && Inv.item.DB.baseDatos[itSuelto.ID].acumulable)
+            {
+                itSuelto.cantidad += this.cantidad;
+            }
+            else
+            {
+                Inv.itemsSueltos.Add(this);
+            }
+            if(!this.gameObject.name.Contains("(Clone)") || itSuelto == null)
+            {
+                transform.SetParent(other.transform);
+            }
+            else
+            {
+                Inv.copiasItemsSueltos.Remove(this);
+                Destroy(this.gameObject);
+            }
+
+           // var jug = other.GetComponent<Jugador>();
+            jug.invLocal.Add(this.gameObject);
+          //  jug.questTrack.VerificarItem(ID);
+
+            gameObject.SetActive(false);
+
         }
     }
 }
--- RPG Proyect 2.0/Assets/Scripts/Loot/LootSystem.cs
+++ RPG Proyect 2.0/Assets/Scripts/Loot/LootSystem.cs
@@ -6,9 +6,126 @@ public class LootSystem : MonoBehaviour
 {
     [SerializeField]
     private LootTable lootTable;
+
+    public static LootSystem Instance { get; private set; }
+
+    [SerializeField]
+    private int probabilidad;
+
+    [SerializeField]
+    private int total;
+
+    private LootTable.Probabilidades[] localProb;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
     void Start()
     {
-        
-    }
+        localProb = lootTable.prob;
+        System.Array.Sort(localProb, new ComparadorDeRarezas());
+        System.Array.Reverse(localProb);
 
+        foreach(var weight in localProb)
+        {
+            total += weight.rareza;
+        }
+    }
+    public void SpawnLoot(Transform spawnPoint, int factorAdicional/*Por si algun mob hay que incrementarle la probabilidad de que suelte cosas mejores(nunca menor que 1*/, int cantidadDeItemsADropear/*por si queremos que dropee mas de 1 item*/, int dropChance/*probabilidad de que suelte items o no*/, bool? repeated = null)
+    {
+        probabilidad = Random.Range(0, (total + 1));
+        int miProb = probabilidad * factorAdicional;
+        if(repeated != null)
+        {
+            if(repeated == true)
+            {
+                for (int i = 0; i < localProb.Length; i++)
+                {
+                    if (miProb <= localProb[i].rareza)
+                    {
+                        GameObject go = Instantiate(localProb[i].reward, spawnPoint.position, Quaternion.identity);
+
+                        //go.GetComponent<ItemSuelto>().Inv = GameObject.FindObjectOfType<Jugador>().inventario.GetComponent<Inventario>(); //para que se ponga en el inventario del jugador
+
+                        if (cantidadDeItemsADropear == 1)
+                        {
+                            return;
+                        }
+                        cantidadDeItemsADropear--;
+                    }
+                    else
+                    {
+                        miProb -= localProb[i].rareza;
+                    }
+                }
+            }
+        }
+        //Check para ver si objeton loot o no, entre un 0 y 100%
+        int dropChanceCalculo = Random.Range(0, 101);
+
+        if(dropChanceCalculo >= dropChance)
+        {
+            print("No hay loot");
+            return;
+        }
+        //fin del check
+
+        if(miProb >= total)
+        {
+            miProb = total;
+        }
+
+        for (int i = 0; i < localProb.Length; i++)
+        {
+            if(miProb <= localProb[i].rareza)
+            {
+                GameObject go = Instantiate(localProb[i].reward, spawnPoint.position, Quaternion.identity);
+
+                //go.GetComponent<ItemSuelto>().Inv = GameObject.FindObjectOfType<Jugador>().inventario.GetComponent<Inventario>(); //para que se ponga en el inventario del jugador
+
+                if (cantidadDeItemsADropear == 1)
+                {
+                    return;
+                }
+                cantidadDeItemsADropear--;
+            }
+            else
+            {
+                miProb -= localProb[i].rareza;
+            }
+        }
+        if(cantidadDeItemsADropear >=  1)
+        {
+            SpawnLoot(spawnPoint, factorAdicional, cantidadDeItemsADropear, dropChance, true);
+        }
+    }
+}
+public class ComparadorDeRarezas : IComparer
+{
+    public int Compare(object x, object y)
+    {
+        int n1 = ((LootTable.Probabilidades)x).rareza;
+        int n2 = ((LootTable.Probabilidades)y).rareza;
+        if(n1 > n2)
+        {
+            return 1;
+        }
+        else if (n1 == n2)
+        {
+            return 0;
+        }
+        else
+        {
+            return -1;
+        }
+    }
 }
